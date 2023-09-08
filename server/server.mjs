c3833b3b47bc60369efce57a175222bb7b7d6a87
ST10084382 <st10084382@vcconnect.edu.za>
2023-09-08 09:51:55 +0200

    Second Commit

--- server/server.mjs
+++ server/server.mjs
@@ -1,7 +1,17 @@
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
-import records from "./routes/record.mjs";
+
+import https from "https";
+import path from "path";
+import fs from "fs";
+
+const options = {
+  key: fs.readFileSync('C:/Users/lab_services_student/apdsicetask2/server/keys/private-key.pem'),      //Change Private Key Path here
+  cert: fs.readFileSync('C:/Users/lab_services_student/apdsicetask2/server/keys/certificate.pem'),            //Change Main Certificate Path here
+  }
+
+  import records from "./routes/record.mjs"; 
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -10,6 +20,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/record", records);
+
+let server = https.createServer(options,app)
+
+app.get('/',(req,res)=>{
+  res.send('HTTPS in ExpressJS')
+})
 
 // start the Express server
 server.listen(PORT, () => {
