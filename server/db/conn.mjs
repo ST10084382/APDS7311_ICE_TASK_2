b2fc56761e8fa6eda794ea59ff239a8da424d18f
ST10084382 <st10084382@vcconnect.edu.za>
2023-09-15 09:09:57 +0200

    Environment Variables

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,6 +1,12 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
-const connectionString = "mongodb+srv://admin:Q1w2e3r4@clusterice.wh1lh2j.mongodb.net/";//process.env.ATLAS_URI || "";
+//const connectionString = "mongodb+srv://admin:Q1w2e3r4@clusterice.wh1lh2j.mongodb.net/";//process.env.ATLAS_URI || "";
+
+
+const connectionString = process.env.MONGO_CONN_STRING;
+console.log(connectionString);
 
 const client = new MongoClient(connectionString);
 
--- server/server.mjs
+++ server/server.mjs
@@ -5,11 +5,20 @@ import "./loadEnvironment.mjs";
 import https from "https";
 import path from "path";
 import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+
+const key = process.env.KEY;
+const cert = process.env.CERT;
+
+console.log(key + " KEY AND CERT " + cert);
 
 const options = {
-  key: fs.readFileSync('C:/Users/lab_services_student/apdsicetask2/server/keys/private-key.pem'),      //Change Private Key Path here
-  cert: fs.readFileSync('C:/Users/lab_services_student/apdsicetask2/server/keys/certificate.pem'),            //Change Main Certificate Path here
-  }
+  //key: fs.readFileSync('C:/Users/lab_services_student/APDS7311/apdsicetask2/server/keys/private-key.pem'),      //Change Private Key Path here
+  //cert: fs.readFileSync('C:/Users/lab_services_student/APDS7311/apdsicetask2/server/keys/certificate.pem'),            //Change Main Certificate Path here
+  key: fs.readFileSync(key), 
+  cert: fs.readFileSync(cert),    
+}
 
   import records from "./routes/record.mjs"; 
 
